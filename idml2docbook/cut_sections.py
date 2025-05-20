54333afc033b15a484daad118cb7a26a6464a7ff
yanntrividic <yann.trividic@hotmail.fr>
2025-05-20 09:34:14 +0200

    Cleaning up cut_sections, adding logs

--- idml2docbook/cut_sections.py
+++ idml2docbook/cut_sections.py
@@ -10,18 +10,20 @@ def get_cuts(file):
     """Returns a list of classes that will allow to
     cut the DocBook file in several
     They have a positive "cut" entry in the map."""
+    logging.info("Retrieving cuts from: " + file)
     cuts = []
     for key, value in getMap(file).items():
         if "cut" in value:
             if "role" in value: cuts.append(value[f"role"])
             else: cuts.append(key)
+    logging.info("Cuts are: " + str(cuts))
     return cuts
 
 def split_docbook(docbook, **options):
+    logging.info("Splitting DocBook file...")
     soup = BeautifulSoup(docbook, "xml")
     cuts = get_cuts(options["map"])
     article_contents = soup.select("article > *")
-    # print(article_contents)
     sections = [[]]
 
     while article_contents:
@@ -37,6 +39,7 @@ def split_docbook(docbook, **options):
         if options["prettify"]: sections[i] = BeautifulSoup(sections[i], "xml").prettify()
         names.append(get_name_from_sections(sections[i]))
 
+    logging.info("Found " + str(len(sections)) + " sections!")
     return names, sections
 
 
@@ -47,40 +50,9 @@ def wrap_xml_in_docbook_schema(xml):
            """</article>""")
 
 def get_name_from_sections(xml):
+    """Turning level 1 sections ids into a filename"""
     soup = BeautifulSoup(xml, "xml")
     sections = soup.select("article > section")
     ids = []
     for section in sections: ids.append(section.attrs["xml:id"])
-    return "_".join(ids[:10]).lower()
-
-if __name__ == "__main__":
-
-    docbook = idml2docbook(sys.argv[1])
-
-    # For debugging
-    # with open(sys.argv[1], "r") as f:
-    #     docbook = f.read()
-
-    soup = BeautifulSoup(docbook, "xml")
-    sections = split_docbook(soup, cuts)
-    # for section in sections: print(section)
-
-    for i, section in enumerate(sections):
-        docbook = wrap_xml_in_docbook_schema("".join(section))
-        name = "{:02d}".format(i) + "_" + get_name_from_sections(docbook)
-        cmd = [
-            os.getenv("PANDOC_EXECUTABLE"),
-            "-f", "docbook",
-            # "-t", "markdown_phpextra",
-            "-t", "native",
-            "--lua-filter=lua-filters/roles-to-classes.lua",
-            "--lua-filter=lua-filters/collapse-sections-into-headers.lua",
-            "--wrap=none",
-            "-o", "output/sections/{}.md".format(name)
-            # "-o", "output/sections/{}.native".format(name)
-        ]
-
-        # with open("output/sections/" + str(i) + ".xml", "w") as file:
-        #     file.write(docbook)
-
-        subprocess.run(cmd, input=docbook.encode('utf-8'))
+    return "_".join(ids).lower()
