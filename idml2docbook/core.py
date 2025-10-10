68bcbb6716281101062a6620c99ff5c525f07cb6
yanntrividic <yann.trividic@hotmail.fr>
2025-10-10 12:19:11 +0200

    Updating core to process return value from turn_direct_formatting_into_custom_roles

--- idml2docbook/core.py
+++ idml2docbook/core.py
@@ -190,7 +190,7 @@ def hubxml2docbook(file, **options):
         tag.extract()
     # <article version="5.0" xml:lang="fr-FR" xmlns="http://docbook.org/ns/docbook">
 
-    if not options["ignore_overrides"]: soup = turn_direct_formatting_into_custom_roles(str(soup))
+    if not options["ignore_overrides"]: soup, _, _ = turn_direct_formatting_into_custom_roles(str(soup))
 
     remove_unnecessary_nodes(soup)
     # remove_unnecessary_layer(soup)
